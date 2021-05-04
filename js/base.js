5e896062d3515f7b2eebfb541d9600f66a46e746
Aaron Mendoza <aaronmendoza1095@gmail.com>
2021-05-03 22:04:06 -0500

    Ejercicios

--- js/base.js
+++ js/base.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+let a=window.prompt("Cual es la variable a");
+let b=window.prompt("Cual es la variable b");
+parseInt(a);
+parseInt(b);
+if(a>b){
+    console.log(a/ b)
+
+
+}else if (a<b){
+    console.log(a+ b);
+}else {
+    console.log(a* b);
+}
---
+++ js/parImpar.js
@@ -0,0 +1,8 @@
+let a=window.prompt();
+parseInt(a);
+if((a%2)==0){
+    console.log("Es par");
+
+}else{
+    console.log("Es impar")
+}
