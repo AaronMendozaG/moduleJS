1406d7a3bb203ab4d636c8b5e562849b48f1eb1a
Aaron Mendoza <aaronmendoza1095@gmail.com>
2021-05-12 22:18:36 -0500

    ADD Class Six

--- js/base.js
+++ js/base.js
@@ -1,4 +1,34 @@
 console.log ("METODOS DE ARRAY")
 
 
-let myArray=['Aaron',8,[1,'Mendoza']] 
+// Ejercicio 1 .forEach()
+// Dado un array con solo números
+// obtener el segundo valor mas alto
+// input: [1,4,3,2,5]
+// output: -> 5
+
+let arrayNumbers=[2,5,6,4,8,3,9,54]
+let highNumber=arrayNumbers[0]
+
+arrayNumbers.forEach((item)=> item>highNumber ? highNumber=item : "")
+console.log(highNumber)
+
+
+
+
+
+// ejercicio 2 .forEach() o .map() o for()
+// función
+// convertir todos los elementos numericos de un array
+// a strings
+// input: [1,2,3,4,5]
+// output -> ['1,','2','3','4','5']
+
+let arrToConvert=[1,6,78,9,0]
+let newArr=''
+  arrToConvert.forEach((item) =>{
+    newArr+= item.toString()
+    
+    })
+    console.log(newArr) 
+
