4d5d18ea82e56dc04ebbc7c892564709857f861f
Aaron Mendoza <aaronmg@Mac-mini-de-Aaron.local>
2021-05-13 17:36:40 -0500

    ADD Homework

---
+++ js/homework.js
@@ -0,0 +1,22 @@
+
+/**
+ * Ejericio 1:
+ * Realizar una funcion que dado un array de strings como parametro
+ * retorne un nuevo array con todos los strings que tienen como maximo 5 caracteres
+ */
+    //let arrayToCut=["dog", "wolf", "family", "eaten", "camping"]
+    //let newItem=[]
+    //const maxFive = arr =>arr.forEach(item=>item.length<=5 ? newItem.push(item):'')
+    //maxFive(arrayToCut)
+
+
+/**
+ * Ejericio 2:
+ * Realizar una funcion que dado un array de nombres de koders como parametro
+ * Retorne un nuevo array con todos koders que tienen mas de 1 nombre
+ * 
+ */
+let kodersNames=["Aaron","Antonio", "Victor Yair", "Luis Carlos", "Victor", "José Omar"]
+let newItem=[]
+const twoNames=arr => arr.forEach(item=> item.includes(" ") === true ? newItem.push(item) : '')
+console.log(twoNames(kodersNames))
