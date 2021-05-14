c900c3a21d896bb37a9f12d34f5c6104a525da52
Aaron Mendoza <aaronmg@Mac-mini-de-Aaron.local>
2021-05-13 22:34:36 -0500

    ADD Class eight

--- js/base.js
+++ js/base.js
@@ -1,34 +1,78 @@
 console.log ("METODOS DE ARRAY")
 
-
-// Ejercicio 1 .forEach()
-// Dado un array con solo números
-// obtener el segundo valor mas alto
-// input: [1,4,3,2,5]
-// output: -> 5
-
-let arrayNumbers=[2,5,6,4,8,3,9,54]
-let highNumber=arrayNumbers[0]
-
-arrayNumbers.forEach((item)=> item>highNumber ? highNumber=item : "")
-console.log(highNumber)
+// .map() -> array computado, tiene la misma longitud del original, transforma cada item en uno nuevo.
 
 
 
+// funcion que dado un array
+// genere un nuevo array con los mismos numeros como strings
+    //let arrNum=[1,2,3,4,5,6]
+    //
+    //const convertArr=arr=>{
+    //  return newItem = arr.map( item=>{
+    //      return item.toString()
+    //    }
+    //    
+    //    )
+      //
+    //}
+    //console.log(convertArr(arrNum))
 
 
-// ejercicio 2 .forEach() o .map() o for()
-// función
-// convertir todos los elementos numericos de un array
-// a strings
-// input: [1,2,3,4,5]
-// output -> ['1,','2','3','4','5']
 
-let arrToConvert=[1,6,78,9,0]
-let newArr=''
-  arrToConvert.forEach((item) =>{
-    newArr+= item.toString()
+      //let names=["aaron","javier","juan"]
+      //
+      //const capitalizerNm=arr=>{
+      //  return neeItem=arr.map(item=>{
+      //    return item.indexOf(0).toUpperCase()
+      //  })
+      //}
+      //console.log(capitalizerNm(names))
+
+
+
+// Dados 2 arrays
+// array de nombres
+// array de apellidos
+// Crear una funcion que reciba los 2 arrays, 
+// y retorne un array con los nombres completos
+
+          //let names=["Aaron","Jorge"]
+          //let surNames=["Mendoza","Camarillo"]
+          //
+          //const concatNames = (names,surNames)=>{
+          //  return completeName=names.map(function (item,index) {
+          //    return item.concat(this[index])
+          //  },surNames)
+          //}
+          //console.log(concatNames(names,surNames))
+
+///////METODO REDUCE//////
+
+      //let arrNum=[1,2,3]
+      //
+      //const toString=arr=>{
+      //  return newArr = arr.reduce((acc,item)=>{
+      //    return acc+item
+      //  },"")
+      //}
+      //console.log(toString(arrNum))
+
+     
+
+    let myCar=
+        [
+          ["Reloj", 500] , 
+          ["Reloj", 300] , 
+          ["Reloj", 1200]  
+        ]
     
-    })
-    console.log(newArr) 
+    const calculeTotal=arr=>{
+        return suma=arr.reduce((acc,item)=>{
+          return acc+item[1]
+        } 
+          
+          ,0)
+    }
 
+    console.log(calculeTotal(myCar))
