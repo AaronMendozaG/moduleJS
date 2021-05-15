c4bc29e88a7279301af0826cd1d0b386327a04cc
Aaron Mendoza <aaronmg@Mac-mini-de-Aaron.local>
2021-05-14 20:14:48 -0500

    ADD Homework

--- js/homework.js
+++ js/homework.js
@@ -1,22 +1,49 @@
 
 /**
- * Ejericio 1:
- * Realizar una funcion que dado un array de strings como parametro
- * retorne un nuevo array con todos los strings que tienen como maximo 5 caracteres
- */
-    //let arrayToCut=["dog", "wolf", "family", "eaten", "camping"]
-    //let newItem=[]
-    //const maxFive = arr =>arr.forEach(item=>item.length<=5 ? newItem.push(item):'')
-    //maxFive(arrayToCut)
+ * 
+ * Ejercicio 1
+ 
+Dado el array [ ["jorge", false] , ["luis", true] , ["Victor", false] ] 
+Calcular el numero de personas que si votaron
+Output -> 1
+Nota: las personas que si votaron tiene true
+*/
+
+            let votantes=[["Aaron",true],["Carlos",true],["Miguel",false]]
+            
+            //const getVotes=arr=>{ totalVotes=arr.reduce((acc,item)=>{
+            //              if (item[1]===true) {
+            //                 acc++
+            //                  
+            //              }
+            //              return acc
+            //    },0)
+            //}
+
+const getVotes=arr=>totalVotes=arr.reduce((acc,item)=>(item[1]===true ? acc++ :'',acc),0)
+  
+            
+            console.log(getVotes(votantes))
+
 
 
 /**
- * Ejericio 2:
- * Realizar una funcion que dado un array de nombres de koders como parametro
- * Retorne un nuevo array con todos koders que tienen mas de 1 nombre
  * 
- */
-let kodersNames=["Aaron","Antonio", "Victor Yair", "Luis Carlos", "Victor", "José Omar"]
-let newItem=[]
-const twoNames=arr => arr.forEach(item=> item.includes(" ") === true ? newItem.push(item) : '')
-console.log(twoNames(kodersNames))
+ * Ejercicio 2
+ 
+    Dado un array con pedidos de un restaurant
+    la estructura por cada pedido es 
+    ['entrada','principal','postre','precio'],
+    let pedidos = [
+        ['ensalada de pepinos','paella','platano', 100'],
+        ['ensalada de tomates','pescado','helado', 120],
+        ['ensalada simple','paella','yogurt', 80],
+        ['ensalada simple','enchiladas','yogurt', 80],
+        ['ensalada cesar','salmón','platano', 100],
+    ]
+    Funcion que devuelve el total de pedidos que tienen como plato principal "paella"
+    Funcion que devuelve los pedidos que tienen como postre "platano" o "helado" 
+    Funcion que devuelve los pedidos que tengen un precio superior a 90 
+    Funcion que devuelve los pedidos que tengen un precio superior a 90 
+*/
+
