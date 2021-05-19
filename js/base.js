a98d2055ae591dc5475cfaa355716d918f504d8f
Aaron Mendoza <aaronmg@Mac-mini-de-Aaron.local>
2021-05-18 23:34:50 -0500

    ADD Class ten

--- js/base.js
+++ js/base.js
@@ -1,46 +1,89 @@
-console.log ("GETTERS AND SETTERS")
+console.log ("DOM")
 
-//METODOS DE OBJETOS 
+//DOCUMENT OBJECT MODEL (document)
+let listKoders=''
+let koders = ['antonio', 'victor', 'rose', 'jorge']
+let lista = document.querySelector('.table-arr tbody')
+koders.forEach( (namekoder, index) => {
+  listKoders += `
+  <tr><th scope="row">${index+1}</th>
+  <td>${namekoder}</td></tr>
+      
+  `
+})
+lista.innerHTML = listKoders
 
 
 
-
-
-
-//EJERCICIOS
-let koders=[
+let kodersObject  = [
   {
-    name:'Aaron',
-    lastName:'Mendoza',
-    age:25,
-    
+      name: 'Aaron',
+      lastName: 'Mendoza',
+      age: 30,
+      github:'@fdsfsdf'
   },
   {
-    name:'Carlos',
-    lastName:'Alcibar',
-    age:25,
-    
+      name:'Carlos',
+      lastName:'Alcibar',
+      age: 30,
+      github:'@dsfdsf'
+  },
+  {
+      name:'Ferdinand',
+      lastName:'Bracho',
+      age: 30,
+      github:'@sdfdsfds'
+  }
+  ,
+  {
+      name:'Ferdinand',
+      lastName:'Bracho',
+      age: 30,
+      github:'@sdfdsfds'
+  }
+  ,
+  {
+      name:'Ferdinand',
+      lastName:'Bracho',
+      age: 30,
+      github:'@sdfdsfds'
+  }
+  ,
+  {
+      name:'Ferdinand',
+      lastName:'Bracho',
+      age: 30,
+      github:'@sdfdsfds'
+  }
+  ,
+  {
+      name:'Ferdinand',
+      lastName:'Bracho',
+      age: 30,
+      github:'@sdfdsfds'
+  }
+  ,
+  {
+      name:'Ferdinand',
+      lastName:'Bracho',
+      age: 30,
+      github:'@sdfdsfds'
   }
 ]
 
-
-
-koders.forEach(item=>{
-  item.fullKoder= function(){
-    return `El koder ${this.name} ${this.lastName} tiene ${this.age}`
-  }
+let listaTwo = document.querySelector('.table-object tbody')
+let listKodersTwo=''
+kodersObject.forEach((item,index)=>{
+  let {name, lastName,age,github} = item
+  listKodersTwo += `
+  <tr class="table-info">
+  <th scope="row ">${index+1}</th>
+  <td>${name}</td>
+  <td>${lastName}</td>
+  <td>${age}</td>
+  <td>${github}</td>
+  </tr>
+      
+  `
 })
-
-koders.forEach(item=>{
-  return console.log (item.fullKoder())
-})
-
-
-//getter, funcion que obtiene el valor de una propiedad
-
-
-
-
-//setter, funcin que establece un valor de una propiedad
-
-
+listaTwo.innerHTML = listKodersTwo
