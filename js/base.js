a20afa73d15c849dd292677e6570c19c216f34a3
Aaron Mendoza <aaronmg@Mac-mini-de-Aaron.local>
2021-05-20 00:09:47 -0500

    ADD Class twelve

--- js/base.js
+++ js/base.js
@@ -1,89 +1,189 @@
-console.log ("DOM")
+console.log('Eventos')
 
-//DOCUMENT OBJECT MODEL (document)
-let listKoders=''
-let koders = ['antonio', 'victor', 'rose', 'jorge']
-let lista = document.querySelector('.table-arr tbody')
-koders.forEach( (namekoder, index) => {
-  listKoders += `
-  <tr><th scope="row">${index+1}</th>
-  <td>${namekoder}</td></tr>
-      
-  `
-})
-lista.innerHTML = listKoders
+// addEventListener
+// preventDefault()
+// stopPropagation()
 
 
 
-let kodersObject  = [
-  {
-      name: 'Aaron',
-      lastName: 'Mendoza',
-      age: 30,
-      github:'@fdsfsdf'
-  },
-  {
-      name:'Carlos',
-      lastName:'Alcibar',
-      age: 30,
-      github:'@dsfdsf'
-  },
-  {
-      name:'Ferdinand',
-      lastName:'Bracho',
-      age: 30,
-      github:'@sdfdsfds'
-  }
-  ,
-  {
-      name:'Ferdinand',
-      lastName:'Bracho',
-      age: 30,
-      github:'@sdfdsfds'
-  }
-  ,
-  {
-      name:'Ferdinand',
-      lastName:'Bracho',
-      age: 30,
-      github:'@sdfdsfds'
-  }
-  ,
-  {
-      name:'Ferdinand',
-      lastName:'Bracho',
-      age: 30,
-      github:'@sdfdsfds'
-  }
-  ,
-  {
-      name:'Ferdinand',
-      lastName:'Bracho',
-      age: 30,
-      github:'@sdfdsfds'
-  }
-  ,
-  {
-      name:'Ferdinand',
-      lastName:'Bracho',
-      age: 30,
-      github:'@sdfdsfds'
-  }
+
+
+// events
+
+// click
+
+// const funcionDeClick = () => {
+//     console.log('click desde event listener')
+// }
+// buttonClick.addEventListener('click', funcionDeClick)
+
+//let buttonClick = document.getElementById('eventlistener')
+//
+//buttonClick.addEventListener('click', () => {
+//    console.log('click desde event listener')
+//})
+//
+//buttonClick.addEventListener('focus', () => {
+//    console.log('focus desde event listener')
+//})
+//
+//buttonClick.addEventListener('mouseenter', () => {
+//    console.log('mouseenter desde event listener')
+//})
+//
+//buttonClick.addEventListener('mousemove', () => {
+//    console.log('mousemove desde event listener')
+//})
+//
+//buttonClick.addEventListener('mouseleave', () => {
+//    console.log('mouseleave desde event listener')
+//})
+
+
+// keyboard events
+
+// inputSearch.addEventListener('keydown', function(){
+//     console.log('keydown')
+// })
+// inputSearch.addEventListener('keypress', function(){
+//     console.log('keypress')
+// })
+
+//let inputSearch = document.getElementById('search')
+//let textSearch = document.getElementById('searchtext')
+//inputSearch.addEventListener('keyup', function(){
+//    let text = inputSearch.value
+//    textSearch.innerText = text 
+//})
+
+
+
+
+
+// keydown -> al presionar
+// keypress -> al llegar al tope de la tecla
+// keyup -> al soltar la tecla
+
+
+
+let koders = [
+    {
+        id: 23,
+        name:'jorge'
+    },
+    {
+        id: 31,
+        name:'Antonio'
+    },
+    {
+        id: 212,
+        name:'fer'
+    },
+    {
+        id: 20,
+        name:'rose'
+    }
+
 ]
+let listKoders = [...koders]
+let enrolledKoders = []
 
-let listaTwo = document.querySelector('.table-object tbody')
-let listKodersTwo=''
-kodersObject.forEach((item,index)=>{
-  let {name, lastName,age,github} = item
-  listKodersTwo += `
-  <tr class="table-info">
-  <th scope="row ">${index+1}</th>
-  <td>${name}</td>
-  <td>${lastName}</td>
-  <td>${age}</td>
-  <td>${github}</td>
-  </tr>
-      
-  `
-})
-listaTwo.innerHTML = listKodersTwo
+// print table koders
+
+const printTableKoders = (arrKoders, selector, type) => {
+    let accKodersList = ''
+    arrKoders.forEach(koder => {
+
+        if(type === 'list'){
+            accKodersList += `
+            <tr class="trkoder_${koder.id}">
+                <td>${koder.id}</td>
+                <td>${koder.name}</td>
+                <td>
+                    <button onclick="enrolledkoder(${koder.id})">Inscribir</button>
+                </td>
+            </tr>
+            `
+        } else {
+            accKodersList += `
+            <tr class="trkoder_${koder.id}">
+                <td>${koder.id}</td>
+                <td>${koder.name}</td>
+                <td>
+                    <button onclick="removekoder(${koder.id})">Dar de baja</button>
+                </td>
+            </tr>
+            `
+        }   
+    })
+    // DOM
+    let sel = document.querySelector(selector)
+    sel.innerHTML = accKodersList
+} 
+
+
+const enrolledkoder = (idKoder) => {
+    // get koder data
+    let koderItem = listKoders.filter(koder => {
+        if(koder.id === idKoder){
+            return koder
+        }
+    })
+    enrolledKoders.push(koderItem[0])
+    console.log(koderItem)
+
+    // filter array without koder
+    let kodersNotDeleted = listKoders.filter(koder => {
+        if(koder.id !== idKoder){
+            return koder
+        }
+    })
+    console.log(kodersNotDeleted)
+    listKoders = [...kodersNotDeleted]
+
+    // lista de koders lado izquierdo
+    console.log(listKoders)
+    // lista de koders lado derecho
+    console.log(enrolledKoders)
+
+    printTableKoders(listKoders, '.table-postulados tbody', 'list')
+    printTableKoders(enrolledKoders, '.table-inscritos tbody', 'enrolled')
+}
+
+
+const removekoder = (idKoder) => {
+    // su codigo aquí
+    // get koder data
+    let koderItemTwo = enrolledKoders.filter(koder => {
+        if(koder.id === idKoder){
+            return koder
+        }
+    })
+    listKoders.push(koderItemTwo[0])
+    
+
+   // filter array without koder
+   let kodersNotDeletedTwo = enrolledKoders.filter(koder => {
+       if(koder.id !== idKoder){
+           return koder
+       }
+   })
+   enrolledKoders = [...kodersNotDeletedTwo]
+
+    // lista de koders lado izquierdo
+    console.log(listKoders)
+    // lista de koders lado derecho
+    console.log(enrolledKoders)
+
+    printTableKoders(listKoders, '.table-postulados tbody', 'list')
+    printTableKoders(enrolledKoders, '.table-inscritos tbody', 'enrolled')
+
+
+}
+
+printTableKoders(listKoders, '.table-postulados', 'list')
+printTableKoders(enrolledKoders, '.table-inscritos tbody', 'enrolled')
+
+
+
+
