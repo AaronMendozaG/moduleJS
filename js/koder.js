478b580d16a2ea6f1cbfd59405b53212a4e13141
Aaron Mendoza <aaronmg@Mac-mini-de-Aaron.local>
2021-05-25 21:30:37 -0500

    ADD Homework

--- js/koder.js
+++ js/koder.js
@@ -1,3 +1,9 @@
+let search = window.location.search
+let start = search.indexOf('=') + 1
+let idKoders = search.slice(start)
+ 
+
+
 const printKoder = (arr) => {
     let acc = ''
     arr.forEach((koder,index) => {
@@ -6,7 +12,7 @@ const printKoder = (arr) => {
         <td scope="row">${index+1}</td>
         <td>${koder[1].name}</td>
         <td><a href="/modifyKoder.html?id=${koder[0]}" class="btn btn-outline-info pr-4">Editar</a></td>
-        <td><a href="/listKoders.html?id=${koder[0]}" class="btn btn-outline-info pr-4" id="btn-delete-koder">Eliminar</a></td>
+        <td><a href="" class="btn btn-outline-info pr-4 deletekoder"  data-id="${koder[0]}">Eliminar</a></td>
         </tr>
         
         `
@@ -15,6 +21,63 @@ const printKoder = (arr) => {
     listKoder.innerHTML = acc
     
 }
+
+
+//let deleteKoderBtn = document.getElementById('btn-delete-koder')
+// if (deleteKoderBtn) {
+//    
+//    deleteKoderBtn.addEventListener('click',(e)=>{
+//        let idKoderObj=e.target.dataset.id
+//        console.log(idKoderObj)
+//        const requestDelete = new XMLHttpRequest()
+//        requestDelete.open('DELETE',`https://python2gtest-default-rtdb.firebaseio.com/koders/${idKoderObj}.json`)
+//        requestDelete.addEventListener('readystatechange', () => {
+//         if(requestDelete.readyState !== 4) {
+//             return 
+//         } else {
+//             if(requestDelete.status >= 200 && requestDelete.status <= 299){
+//                 const response  = requestDelete
+//                 const objectResponse = JSON.parse(response.responseText)
+//                 console.log(objectResponse)
+//             } else {
+//                 console.log('No se pudo ejecutar')
+//             }
+//         } 
+//     })
+//     requestDelete.send()
+//    })
+// }
+
+
+let listkoders = document.querySelector('.list-koders tbody')
+listkoders.addEventListener('click', (e)=> {
+    e.preventDefault()
+    console.log(e.target.dataset.id)
+    if(e.target.classList.contains('deletekoder')) {
+        let idkoder = e.target.dataset.id
+
+        const requestDelete = new XMLHttpRequest()
+        requestDelete.open('DELETE',`https://python2gtest-default-rtdb.firebaseio.com/koders/${idkoder}.json`)
+        requestDelete.addEventListener('readystatechange', () => {
+            if(requestDelete.readyState !== 4) {
+                return 
+            } else {
+                if(requestDelete.status >= 200 && requestDelete.status <= 299){
+
+                    const objectResponse = JSON.parse(requestDelete.responseText)
+                    window.location.reload()
+                    
+                } else {
+                    console.log('No se pudo ejecutar')
+                }
+            } 
+        })
+        requestDelete.send()
+        
+    }
+})
+
+
 
 
 const request = new XMLHttpRequest()
@@ -26,8 +89,6 @@ request.addEventListener('readystatechange', () => {
         if(request.status >= 200 && request.status <= 299){
             const response  = request
             const objectResponse = JSON.parse(response.responseText)
-            console.log(objectResponse)
-            console.log(Object.entries(objectResponse))
             printKoder(Object.entries(objectResponse))
         } else {
             console.log('No se pudo ejecutar')
@@ -43,34 +104,7 @@ request.send()
 
 
 
-let search = window.location.search
-let start = search.indexOf('=') + 1
-let idKoders = search.slice(start)
 
-console.log(typeof(idKoders))
- let deleteKoderBtn = document.getElementById('btn-delete-koder')
- if (deleteKoderBtn) {
-    
-    deleteKoderBtn.addEventListener('click',()=>{
-        
-        const requestDelete = new XMLHttpRequest()
-     requestDelete.open('DELETE',`https://python2gtest-default-rtdb.firebaseio.com/koders/${idKoders}.json`)
-     requestDelete.addEventListener('readystatechange', () => {
-         if(requestDelete.readyState !== 4) {
-             return 
-         } else {
-             if(requestDelete.status >= 200 && requestDelete.status <= 299){
-                 const response  = requestDelete
-                 const objectResponse = JSON.parse(response.responseText)
-                 console.log(objectResponse)
-             } else {
-                 console.log('No se pudo ejecutar')
-             }
-         } 
-     })
-     requestDelete.send()
-    })
- }
 
 
  
